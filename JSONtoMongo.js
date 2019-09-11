08ca4425a21192ce028b4ecfafddb7e802bb1f6d
Hunter Jarrell <hjarrell555@gmail.com>
2019-09-11 14:04:15 -0400

    Complete Bootcamp2
    Update all assignment files

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -12,6 +12,7 @@ var fs = require('fs'),
 /* Connect to your database using mongoose - remember to keep your key secret*/
 //see https://mongoosejs.com/docs/connections.html
 //See https://docs.atlas.mongodb.com/driver-connection/
+mongoose.connect(config.db.uri, {useNewUrlParser: true, dbName: 'listings'});
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
@@ -20,9 +21,10 @@ var fs = require('fs'),
 
   Remember that we needed to read in a file like we did in Bootcamp Assignment #1.
  */
+fs.readFile('listings.json', 'utf8', (err, data) => {
+  const listing_data = JSON.parse(data);
 
-
-/*  
-  Check to see if it works: Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
- */
+  listing_data.entries.forEach((listing) => {
+    (new Listing(listing)).save();
+  });
+});
--- queries.js
+++ queries.js
@@ -1,7 +1,9 @@
 /* Add all the required libraries*/
-
+const mongoose = require('mongoose'),
+  Listing = require('./ListingSchema.js'), 
+  config = require('./config');
 /* Connect to your database using mongoose - remember to keep your key secret*/
-
+mongoose.connect(config.db.uri, {useNewUrlParser: true, dbName: 'listings'});
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
 
@@ -10,6 +12,11 @@ var findLibraryWest = function() {
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.findOne({
+    code: "LBW",
+  }, (error, listing) => {
+    console.log(listing);
+  })
 };
 var removeCable = function() {
   /*
@@ -17,17 +24,33 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+  Listing.findOneAndRemove({
+    code: "CABL",
+  }, (error, listing) => {
+    console.log(listing);
+  })
 };
 var updatePhelpsMemorial = function() {
   /*
     Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+  Listing.findOneAndUpdate({
+    code: "PHL"
+  },
+  {
+    address: "1953 Museum Rd, Gainesville, FL 32603"
+  }, (error, listing) => {
+    console.log(listing)
+  })
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+  Listing.find((error, listings) => {
+    listings.forEach((listing) => console.log(listing));
+  });
 };
 
 findLibraryWest();
